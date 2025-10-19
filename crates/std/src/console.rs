85e3fd772428c9b0da1f23f34e244a8e3592687f
Ellie Gummere <me@disilla.org>
2025-10-19 14:32:48 -0400

    feat(std): update console module to use new sys println

--- crates/std/src/console.rs
+++ crates/std/src/console.rs
@@ -5,10 +5,8 @@
 #[no_mangle]
 pub extern "C" fn oats_std_console_log(msg: *mut libc::c_char) {
     if !msg.is_null() {
-        // For now, print directly. In a real implementation,
-        // we'd wrap this in an OatsString and use safe methods
         unsafe {
-            crate::sys::print_string(std::ffi::CStr::from_ptr(msg).to_string_lossy().as_ref());
+            crate::sys::println(std::ffi::CStr::from_ptr(msg).to_string_lossy().as_ref());
         }
     }
 }
@@ -17,19 +15,19 @@ pub extern "C" fn oats_std_console_log(msg: *mut libc::c_char) {
 /// #[oats_export]
 #[no_mangle]
 pub extern "C" fn oats_std_console_log_number(num: f64) {
-    crate::sys::print_number_f64(num);
+    crate::sys::println(num);
 }
 
 /// Log an integer to the console
 /// #[oats_export]
 #[no_mangle]
 pub extern "C" fn oats_std_console_log_int(num: i32) {
-    crate::sys::print_number_i32(num);
+    crate::sys::println(num);
 }
 
 /// Print a newline
 /// #[oats_export]
 #[no_mangle]
 pub extern "C" fn oats_std_console_log_newline() {
-    crate::sys::print_nl();
+    crate::sys::println("");
 }
